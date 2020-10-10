09fdfd0252bf61b399ce46aa1be98a16a981e3ef
khantkyawhtet3002 <69779607+khantkyawhtet3002@users.noreply.github.com>
2020-10-10 23:25:34 +0630

    to_show_image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
-  config.assets.compile = false
+  config.assets.compile = true
   config.active_storage.service = :local
   config.log_level = :debug
   config.log_tags = [ :request_id ]
