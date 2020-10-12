bf4a4222cf57b1313efa8f61b6e609208fb6c324
khantkyawhtet3002 <69779607+khantkyawhtet3002@users.noreply.github.com>
2020-10-12 12:03:06 +0630

    pic_contro

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -58,12 +58,11 @@ class PicturesController < ApplicationController
     end
 
   private
-  
-    def picture_params
-      params.require(:picture).permit(:title, :content, :image, :image_cache, :remove_image)
-    end
-
-    def set_picture
-      @picture = Picture.find(params[:id])
-    end
+  def picture_params
+     params.require(:picture).permit(:title, :content, :image, :image_cache, :remove_image)
   end
+
+  def set_picture
+     @picture = Picture.find(params[:id])
+  end
+ end
